977064f2e74916f2a36d40c962619bd78fe2b132
duarte15 <leladscs@hotmail.com>
2017-04-04 17:24:45 -0300

    Questões do vídeo

---
+++ 1aQuestao.py
@@ -0,0 +1,24 @@
+1ª Questão
+''' Escreva uma função recursiva que  imprima de um número x até um y.'''
+
+def recur(x,y):
+
+  print(x)
+
+  if (x == y):
+
+    return("")
+
+  return recur(x+1,y)
+
+x=int(input("Informe o valor de x: "))
+
+y=int(input("Informe o valor de y(maior que x): "))
+
+if (x<y):
+
+  recur(x,y)
+
+else:
+
+  print("O valor de x deve ser menor que o valor de y!")
---
+++ 2aQuestao.py
@@ -0,0 +1,22 @@
+2ª Questão
+''' Escreva uma função recursiva que retorna a soma de n até zero.'''
+
+soma = 0
+
+def recur(n):
+
+  global soma
+
+  soma+= n
+
+  if (n == 0):
+
+    return soma
+
+  return recur(n-1)
+
+
+
+n=int(input("Informe o valor de n: "))
+
+print(recur(n))
